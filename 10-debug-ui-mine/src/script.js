1abdb2cfe55609a2e49b91f81f66ce9f068332b8
Evan Lask <elask@apple.com>
2021-07-08 12:04:39 -0600

    Finished lesson 10

--- 10-debug-ui-mine/src/script.js
+++ 10-debug-ui-mine/src/script.js
@@ -1,7 +1,21 @@
+import * as dat from 'dat.gui';
+import gsap from 'gsap';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 import './style.css';
+
+// Debug
+const gui = new dat.GUI();
+const parameters = {
+  color: 0xffff00,
+  spin: () => {
+    gsap.to(mesh.rotation, {
+      duration: 2,
+      y: mesh.rotation.y + Math.PI * 2,
+    });
+  },
+};
 
 // Dimensions
 const size = {
@@ -23,13 +37,20 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 const scene = new THREE.Scene();
 
 // Cube
-const mesh = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
-  new THREE.MeshBasicMaterial({
-    color: 0xff0000,
-  })
-);
+const geometry = new THREE.BoxGeometry(1, 1, 1, 1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: parameters.color });
+const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
+
+gui.addColor(parameters, 'color').onChange(() => {
+  material.color.set(parameters.color);
+});
+gui.add(mesh.position, 'x').min(-3).max(3).step(0.01).name('position x');
+gui.add(mesh.position, 'y').min(-3).max(3).step(0.01).name('position y');
+gui.add(mesh.position, 'z').min(-3).max(3).step(0.01).name('position z');
+gui.add(mesh, 'visible');
+gui.add(material, 'wireframe');
+gui.add(parameters, 'spin');
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 100);
@@ -39,7 +60,7 @@ scene.add(camera);
 // Controls
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
-controls.target = mesh.position;
+// controls.target = mesh.position;
 controls.update();
 
 // Animate
@@ -77,24 +98,24 @@ window.addEventListener('resize', () => {
 });
 
 // Handle fullscreen
-window.addEventListener('dblclick', () => {
-  // Are we in fullscreen?
-  const isFullscreen = document.fullscreenElement || document.webkitFullscreenElement;
+// window.addEventListener('dblclick', () => {
+//   // Are we in fullscreen?
+//   const isFullscreen = document.fullscreenElement || document.webkitFullscreenElement;
 
-  // Enter fullscreen
-  if (!isFullscreen) {
-    if (renderer.domElement.requestFullscreen) {
-      renderer.domElement.requestFullscreen();
-    } else {
-      renderer.domElement.webkitRequestFullscreen();
-    }
-  }
-  // Exit fullscreen
-  else {
-    if (document.exitFullscreen) {
-      document.exitFullscreen();
-    } else {
-      document.webkitExitFullscreen();
-    }
-  }
-});
+//   // Enter fullscreen
+//   if (!isFullscreen) {
+//     if (renderer.domElement.requestFullscreen) {
+//       renderer.domElement.requestFullscreen();
+//     } else {
+//       renderer.domElement.webkitRequestFullscreen();
+//     }
+//   }
+//   // Exit fullscreen
+//   else {
+//     if (document.exitFullscreen) {
+//       document.exitFullscreen();
+//     } else {
+//       document.webkitExitFullscreen();
+//     }
+//   }
+// });

69df233db1a772fd03a55e318091cb022bbeed63
Evan Lask <elask@apple.com>
2021-07-08 17:12:21 -0600

    Added lesson 13

---
+++ 13-3d-text-mine/src/script.js
@@ -0,0 +1,85 @@
+import * as dat from 'dat.gui';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+import './style.css';
+
+// Textures
+const loadingManager = new THREE.LoadingManager();
+
+loadingManager.onError = () => {
+  console.log('error');
+};
+
+const textureLoader = new THREE.TextureLoader(loadingManager);
+
+// Debug
+const gui = new dat.GUI();
+
+// Dimensions
+const size = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+let aspectRatio = size.width / size.height;
+
+// Renderer
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer({
+  antialias: true,
+  canvas,
+});
+renderer.setSize(size.width, size.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// Scene
+const scene = new THREE.Scene();
+
+// Objects
+const cube = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial());
+scene.add(cube);
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 100);
+camera.position.x = 1;
+camera.position.y = 1;
+camera.position.z = 2;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
+// Animate
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
+
+// Handle resize
+window.addEventListener('resize', () => {
+  // Update dimensions
+  size.width = window.innerWidth;
+  size.height = window.innerHeight;
+  aspectRatio = size.width / size.height;
+
+  // Update camera
+  camera.aspect = aspectRatio;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(size.width, size.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});

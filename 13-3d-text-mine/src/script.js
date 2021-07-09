37423d4da6c1a6e312074e7e4fd526daee15c2f9
Evan Lask <elask@apple.com>
2021-07-09 15:06:31 -0600

    Finished lesson 13

--- 13-3d-text-mine/src/script.js
+++ 13-3d-text-mine/src/script.js
@@ -4,6 +4,9 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 import './style.css';
 
+// Debug
+const gui = new dat.GUI();
+
 // Textures
 const loadingManager = new THREE.LoadingManager();
 
@@ -12,9 +15,65 @@ loadingManager.onError = () => {
 };
 
 const textureLoader = new THREE.TextureLoader(loadingManager);
+const textTexture = textureLoader.load('/textures/matcaps/test2.png');
+const donutTexture = textureLoader.load('/textures/matcaps/4.png');
 
-// Debug
-const gui = new dat.GUI();
+// Fonts
+const fontLoader = new THREE.FontLoader();
+
+fontLoader.load('/fonts/gentilis_bold.typeface.json', (font) => {
+  // Text
+  const bevelSize = 0.01;
+  const bevelThickness = 0.01;
+
+  const textGeometry = new THREE.TextGeometry('PEW PEW!!!', {
+    font: font,
+    size: 0.5,
+    height: 0.2,
+    curveSegments: 12,
+    bevelEnabled: true,
+    bevelThickness,
+    bevelSize,
+    bevelOffset: 0,
+    bevelSegments: 12,
+  });
+  textGeometry.center();
+  textGeometry.computeBoundingBox();
+
+  console.log(textGeometry.boundingBox);
+
+  const textMaterial = new THREE.MeshMatcapMaterial();
+  textMaterial.matcap = textTexture;
+
+  const text = new THREE.Mesh(textGeometry, textMaterial);
+
+  scene.add(text);
+
+  // Donuts
+  const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45);
+  const donutMaterial = new THREE.MeshMatcapMaterial();
+  donutMaterial.matcap = donutTexture;
+
+  const donutCount = 1000;
+  const donutRange = 30;
+  for (let i = 0; i < donutCount; i++) {
+    const donut = new THREE.Mesh(donutGeometry, donutMaterial);
+
+    donut.position.x = (Math.random() - 0.5) * donutRange;
+    donut.position.y = (Math.random() - 0.5) * donutRange;
+    donut.position.z = (Math.random() - 0.5) * donutRange;
+
+    donut.rotation.x = Math.random() * Math.PI;
+    donut.rotation.y = Math.random() * Math.PI;
+
+    const scale = Math.random();
+    donut.scale.x = scale;
+    donut.scale.y = scale;
+    donut.scale.z = scale;
+
+    scene.add(donut);
+  }
+});
 
 // Dimensions
 const size = {
@@ -35,12 +94,12 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 // Scene
 const scene = new THREE.Scene();
 
-// Objects
-const cube = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial());
-scene.add(cube);
+// Axes Helper
+// const axesHelper = new THREE.AxesHelper();
+// scene.add(axesHelper);
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 100);
+const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.05, 100);
 camera.position.x = 1;
 camera.position.y = 1;
 camera.position.z = 2;

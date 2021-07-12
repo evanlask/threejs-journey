bea05d1b14a5ceab049341275aedcf35dab12027
Evan Lask <elask@apple.com>
2021-07-12 10:58:38 -0600

    Added lesson 14

---
+++ 14-lights-mine/src/script.js
@@ -0,0 +1,109 @@
+import * as dat from 'dat.gui';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+import './style.css';
+
+// Debug
+const gui = new dat.GUI();
+
+// Dimensions
+const size = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+let aspectRatio = size.width / size.height;
+
+// Renderer
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer({
+  antialias: true,
+  canvas,
+});
+renderer.setSize(size.width, size.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// Scene
+const scene = new THREE.Scene();
+
+// Lights
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+
+const pointLight = new THREE.PointLight(0xffffff, 0.5);
+pointLight.position.x = 2;
+pointLight.position.y = 3;
+pointLight.position.z = 4;
+scene.add(pointLight);
+
+// Objects
+const material = new THREE.MeshStandardMaterial();
+material.roughness = 0.4;
+
+const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 32, 32), material);
+sphere.position.x = -1.5;
+
+const cube = new THREE.Mesh(new THREE.BoxGeometry(0.75, 0.75, 0.75), material);
+
+const torus = new THREE.Mesh(new THREE.TorusGeometry(0.3, 0.2, 32, 64), material);
+torus.position.x = 1.5;
+
+const plane = new THREE.Mesh(new THREE.PlaneGeometry(5, 5), material);
+plane.rotation.x = -Math.PI * 0.5;
+plane.position.y = -0.65;
+
+scene.add(sphere, cube, torus, plane);
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.05, 100);
+camera.position.x = 1;
+camera.position.y = 1;
+camera.position.z = 2;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
+// Animate
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update objects
+  sphere.rotation.y = 0.1 * elapsedTime;
+  cube.rotation.y = 0.1 * elapsedTime;
+  torus.rotation.y = 0.1 * elapsedTime;
+
+  sphere.rotation.x = 0.15 * elapsedTime;
+  cube.rotation.x = 0.15 * elapsedTime;
+  torus.rotation.x = 0.15 * elapsedTime;
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
+
+// Handle resize
+window.addEventListener('resize', () => {
+  // Update dimensions
+  size.width = window.innerWidth;
+  size.height = window.innerHeight;
+  aspectRatio = size.width / size.height;
+
+  // Update camera
+  camera.aspect = aspectRatio;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(size.width, size.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});

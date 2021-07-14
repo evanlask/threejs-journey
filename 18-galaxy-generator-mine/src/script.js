03e6631aeee9edac617fbdd8a49d868d51c9bbe0
Evan Lask <elask@apple.com>
2021-07-14 13:55:59 -0600

    Added textures and rotation

--- 18-galaxy-generator-mine/src/script.js
+++ 18-galaxy-generator-mine/src/script.js
@@ -11,6 +11,7 @@ const gui = new dat.GUI({
 
 // Textures
 const textureLoader = new THREE.TextureLoader();
+const particleTexture = textureLoader.load('/particles/8.png');
 
 // Dimensions
 const size = {
@@ -50,8 +51,11 @@ const parameters = {
   radius: 5,
   randomness: 0.4,
   randomnessPower: 3,
+  rotation: true,
+  rotationSpeed: 0.05,
   size: 0.01,
   spin: 1,
+  texture: false,
 };
 
 let geometry = null;
@@ -113,11 +117,16 @@ const generateGalaxy = () => {
   geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
 
   material = new THREE.PointsMaterial();
+  material.blending = THREE.AdditiveBlending;
+  material.depthWrite = false;
   material.size = parameters.size;
   material.sizeAttenuation = true;
-  material.depthWrite = false;
-  material.blending = THREE.AdditiveBlending;
   material.vertexColors = true;
+
+  if (parameters.texture) {
+    material.alphaMap = particleTexture;
+    material.transparent = true;
+  }
 
   points = new THREE.Points(geometry, material);
   scene.add(points);
@@ -125,18 +134,21 @@ const generateGalaxy = () => {
 
 generateGalaxy();
 
-gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy);
+gui.add(parameters, 'branches').min(2).max(10).step(1).onFinishChange(generateGalaxy);
 gui.addColor(parameters, 'colorInside').onFinishChange(generateGalaxy);
 gui.addColor(parameters, 'colorOutside').onFinishChange(generateGalaxy);
 gui.add(parameters, 'count').min(100).max(1000000).step(100).onFinishChange(generateGalaxy);
 gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy);
 gui.add(parameters, 'randomness').min(0).max(2).step(0.001).onFinishChange(generateGalaxy);
 gui.add(parameters, 'randomnessPower').min(1).max(10).step(0.001).onFinishChange(generateGalaxy);
+gui.add(parameters, 'rotation');
+gui.add(parameters, 'rotationSpeed').min(-5).max(5).step(0.0001);
 gui.add(parameters, 'size').min(0.001).max(0.1).step(0.001).onFinishChange(generateGalaxy);
 gui.add(parameters, 'spin').min(-2).max(2).step(0.001).onFinishChange(generateGalaxy);
+gui.add(parameters, 'texture').onFinishChange(generateGalaxy);
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.05, 100);
+const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.05, 20);
 camera.position.x = 3;
 camera.position.y = 3;
 camera.position.z = 3;
@@ -148,9 +160,17 @@ controls.enableDamping = true;
 
 // Animate
 const clock = new THREE.Clock();
+let lastTime = clock.getElapsedTime();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - lastTime;
+  lastTime = elapsedTime;
+
+  // Update galaxy
+  if (parameters.rotation && points) {
+    points.rotation.y = points.rotation.y + deltaTime * parameters.rotationSpeed;
+  }
 
   // Update controls
   controls.update();

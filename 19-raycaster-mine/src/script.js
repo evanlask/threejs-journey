1777bb6a7bd842e2ab44f67b013950cac63efd52
Evan Lask <elask@apple.com>
2021-07-15 10:46:02 -0600

    Added lesson 19

---
+++ 19-raycaster-mine/src/script.js
@@ -0,0 +1,103 @@
+import * as dat from 'dat.gui';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+import './style.css';
+
+// Debug
+const gui = new dat.GUI();
+
+// Textures
+const textureLoader = new THREE.TextureLoader();
+
+// Dimensions
+const size = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+let aspectRatio = size.width / size.height;
+
+// Renderer
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer({
+  antialias: true,
+  canvas,
+});
+// renderer.shadowMap.enabled = true;
+// renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.setSize(size.width, size.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// Scene
+const scene = new THREE.Scene();
+
+// Axes Helper
+const axesHelper = new THREE.AxesHelper(10);
+scene.add(axesHelper);
+
+// Grid Helper
+const gridHelper = new THREE.GridHelper(20, 20);
+scene.add(gridHelper);
+
+// Objects
+const object1 = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 16, 16),
+  new THREE.MeshBasicMaterial({ color: '#ff0000' })
+);
+object1.position.x = -2;
+
+const object2 = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 16, 16),
+  new THREE.MeshBasicMaterial({ color: '#ff0000' })
+);
+
+const object3 = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 16, 16),
+  new THREE.MeshBasicMaterial({ color: '#ff0000' })
+);
+object3.position.x = 2;
+
+scene.add(object1, object2, object3);
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.05, 100);
+camera.position.z = 3;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
+// Animate
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
+
+// Handle resize
+window.addEventListener('resize', () => {
+  // Update dimensions
+  size.width = window.innerWidth;
+  size.height = window.innerHeight;
+  aspectRatio = size.width / size.height;
+
+  // Update camera
+  camera.aspect = aspectRatio;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(size.width, size.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});

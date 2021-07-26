6c92907a799b1b1eccb964dc1638a7c203b3214b
Evan Lask <elask@apple.com>
2021-07-26 11:25:37 -0600

    Added lesson 24

---
+++ 24-shaders-mine/src/script.js
@@ -0,0 +1,93 @@
+import * as dat from 'dat.gui';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import * as THREE from 'three';
+
+import './style.css';
+
+// Debug
+const gui = new dat.GUI();
+
+// Textures
+const textureLoader = new THREE.TextureLoader();
+
+// Dimensions
+const size = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+let aspectRatio = size.width / size.height;
+
+// Renderer
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer({
+  antialias: true,
+  canvas,
+});
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.setSize(size.width, size.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// Scene
+const scene = new THREE.Scene();
+
+// Axes Helper
+const axesHelper = new THREE.AxesHelper(10);
+scene.add(axesHelper);
+
+// Geometry
+const geometry = new THREE.PlaneGeometry(1, 1, 32, 32);
+
+// Material
+const material = new THREE.MeshBasicMaterial();
+
+// Mesh
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.05, 100);
+camera.position.set(0.25, -0.25, 1);
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+// Animate
+const clock = new THREE.Clock();
+let lastElapsedTime = clock.getElapsedTime();
+let currentIntersect = null;
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+  const deltaElapsedTime = elapsedTime - lastElapsedTime;
+  lastElapsedTime = elapsedTime;
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
+
+// Handle resize
+window.addEventListener('resize', () => {
+  // Update dimensions
+  size.width = window.innerWidth;
+  size.height = window.innerHeight;
+  aspectRatio = size.width / size.height;
+
+  // Update camera
+  camera.aspect = aspectRatio;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(size.width, size.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});

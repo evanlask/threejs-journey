d3abfe9e8f999eac46e5e3976bd23d5b61242c6b
Evan Lask <elask@apple.com>
2021-08-16 15:10:47 -0600

    Pattern 14

--- 25-shader-patterns-mine/src/shaders/test/fragment.glsl
+++ 25-shader-patterns-mine/src/shaders/test/fragment.glsl
@@ -79,9 +79,22 @@ varying vec2 vUv;
 // }
 
 // Pattern 13
+// void main() {
+//     float strength  = step(0.4, mod(vUv.x * 10.0, 1.0));
+//     strength *= step(0.8, mod(vUv.y * 10.0, 1.0));
+
+//     gl_FragColor = vec4(vec3(strength), 1.0);
+// }
+
+// Pattern 14
 void main() {
-    float strength  = step(0.4, mod(vUv.x * 10.0, 1.0));
-    strength *= step(0.8, mod(vUv.y * 10.0, 1.0));
+    float barX  = step(0.4, mod(vUv.x * 10.0, 1.0));
+    barX *= step(0.8, mod(vUv.y * 10.0, 1.0));
+
+    float barY  = step(0.8, mod(vUv.x * 10.0, 1.0));
+    barY *= step(0.4, mod(vUv.y * 10.0, 1.0));
+
+    float strength = barX + barY;
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }

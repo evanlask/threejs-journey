af7e17186c096934974f299edcde28ed3401f281
Evan Lask <elask@apple.com>
2021-08-17 14:27:14 -0600

    Pattern 39

--- 25-shader-patterns-mine/src/shaders/test/fragment.glsl
+++ 25-shader-patterns-mine/src/shaders/test/fragment.glsl
@@ -322,10 +322,26 @@ vec2 rotate(vec2 uv, float rotation, vec2 mid) {
 // }
 
 // Pattern 38
+// void main() {
+//     vec2 wavedUv = vec2(
+//         vUv.x + sin(vUv.y * 30.0) * 0.1,
+//         vUv.y + sin(vUv.x * 30.0) * 0.1
+//     );
+
+//     float strength = abs(distance(wavedUv, vec2(0.5)) - 0.25);
+    
+//     strength = step(0.01, strength);
+
+//     strength = 1.0 - strength;
+
+//     gl_FragColor = vec4(vec3(strength), 1.0);
+// }
+
+// Pattern 39
 void main() {
     vec2 wavedUv = vec2(
-        vUv.x + sin(vUv.y * 30.0) * 0.1,
-        vUv.y + sin(vUv.x * 30.0) * 0.1
+        vUv.x + sin(vUv.y * 100.0) * 0.1,
+        vUv.y + sin(vUv.x * 100.0) * 0.1
     );
 
     float strength = abs(distance(wavedUv, vec2(0.5)) - 0.25);

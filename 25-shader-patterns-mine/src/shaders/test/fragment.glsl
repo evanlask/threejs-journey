4372301a3df5bdb9fa7a32be4d9fe2fbda590907
Evan Lask <elask@apple.com>
2021-08-17 13:45:11 -0600

    Pattern 31

--- 25-shader-patterns-mine/src/shaders/test/fragment.glsl
+++ 25-shader-patterns-mine/src/shaders/test/fragment.glsl
@@ -230,13 +230,21 @@ float random(vec2 st) {
 // }
 
 // Pattern 30
+// void main() {
+//     float strength = 0.15 / (
+//         distance(
+//             vec2(vUv.x, (vUv.y - 0.5) * 5.0 + 0.5),
+//             vec2(0.5)
+//         )
+//     );
+
+//     gl_FragColor = vec4(vec3(strength), 1.0);
+// }
+
+// Pattern 31
 void main() {
-    float strength = 0.15 / (
-        distance(
-            vec2(vUv.x, (vUv.y - 0.5) * 5.0 + 0.5),
-            vec2(0.5)
-        )
-    );
+    float strength = 0.15 / (distance(vec2(vUv.x, (vUv.y - 0.5) * 5.0 + 0.5), vec2(0.5)));
+    strength *= 0.15 / (distance(vec2(vUv.y, (vUv.x - 0.5) * 5.0 + 0.5), vec2(0.5)));
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }

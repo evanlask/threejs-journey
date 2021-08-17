d6985ddb4cbfa8cab7a5417f1b6a85dff495fde6
Evan Lask <elask@apple.com>
2021-08-17 14:57:28 -0600

    Pattern 49

--- 25-shader-patterns-mine/src/shaders/test/fragment.glsl
+++ 25-shader-patterns-mine/src/shaders/test/fragment.glsl
@@ -472,8 +472,15 @@ float cnoise(vec2 P) {
 // }
 
 // Pattern 48
+// void main() {
+//     float strength = 1.0 - abs(cnoise(vUv * 10.0));
+
+//     gl_FragColor = vec4(vec3(strength), 1.0);
+// }
+
+// Pattern 49
 void main() {
-    float strength = 1.0 - abs(cnoise(vUv * 10.0));
+    float strength = sin(cnoise(vUv * 10.0) * 20.0);
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }

ef8aa844234ed28de9486909ceed0177b66b3638
Evan Lask <elask@logitech.com>
2023-01-13 16:04:44 -0700

    Lesson 46 Clone

--- load-models-with-r3f/src/Model.jsx
+++ load-models-with-r3f/src/Model.jsx
@@ -1,9 +1,17 @@
-import { useGLTF } from '@react-three/drei';
+import { useGLTF, Clone } from '@react-three/drei';
 
 const Model = () => {
   const model = useGLTF('./hamburger.glb');
 
-  return <primitive object={model.scene} scale={0.35} />;
+  return (
+    <>
+      <Clone object={model.scene} position-x={-4} scale={0.35} />
+      <Clone object={model.scene} position-x={0} scale={0.35} />
+      <Clone object={model.scene} position-x={4} scale={0.35} />
+    </>
+  );
 };
+
+useGLTF.preload('./hamburger.glb');
 
 export default Model;

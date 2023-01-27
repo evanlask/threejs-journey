8e3f8e79ec6aa0541f2d9dcacb4196e6a043d9ce
Evan Lask <elask@logitech.com>
2023-01-27 16:19:44 -0700

    Lesson 49 complete

--- mouse-events-with-r3f/src/Experience.jsx
+++ mouse-events-with-r3f/src/Experience.jsx
@@ -1,11 +1,13 @@
-import { useFrame } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
 import { useRef } from 'react';
+import { meshBounds, useGLTF, OrbitControls } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
 
 export default function Experience() {
   const cube = useRef();
 
-  useFrame((state, delta) => {
+  const hamburger = useGLTF('./hamburger.glb');
+
+  useFrame((_, delta) => {
     cube.current.rotation.y += delta * 0.2;
   });
 
@@ -13,20 +15,49 @@ export default function Experience() {
     <>
       <OrbitControls makeDefault />
 
-      <directionalLight position={[1, 2, 3]} intensity={1.5} />
+      <directionalLight intensity={1.5} position={[1, 2, 3]} />
       <ambientLight intensity={0.5} />
 
-      <mesh position-x={-2}>
+      <mesh
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+        position-x={-2}
+      >
         <sphereGeometry />
         <meshStandardMaterial color="orange" />
       </mesh>
 
-      <mesh ref={cube} position-x={2} scale={1.5}>
+      <mesh
+        onClick={() => {
+          cube.current.material.color.set(`hsl(${Math.random() * 360}, 100%, 75%)`);
+        }}
+        onPointerEnter={() => {
+          document.body.style.cursor = 'pointer';
+        }}
+        onPointerLeave={() => {
+          document.body.style.cursor = 'default';
+        }}
+        position-x={2}
+        raycast={meshBounds}
+        ref={cube}
+        scale={1.5}
+      >
         <boxGeometry />
         <meshStandardMaterial color="mediumpurple" />
       </mesh>
 
-      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+      <primitive
+        object={hamburger.scene}
+        onClick={(e) => {
+          console.log(e.object.name);
+          e.stopPropagation();
+        }}
+        position-y={0.5}
+        scale={0.25}
+      />
+
+      <mesh rotation-x={-Math.PI * 0.5} position-y={-1} scale={10}>
         <planeGeometry />
         <meshStandardMaterial color="greenyellow" />
       </mesh>

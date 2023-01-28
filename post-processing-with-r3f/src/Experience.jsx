5455f51276e84d3fb3576800fec17bda37bc7976
Evan Lask <elask@logitech.com>
2023-01-27 17:04:53 -0700

    Lesson 50 bloom

--- post-processing-with-r3f/src/Experience.jsx
+++ post-processing-with-r3f/src/Experience.jsx
@@ -1,9 +1,20 @@
-import { OrbitControls } from '@react-three/drei';
+import { BlendFunction, GlitchMode } from 'postprocessing';
 import { Perf } from 'r3f-perf';
+import { OrbitControls } from '@react-three/drei';
+import { Bloom, EffectComposer, Glitch, Noise, Vignette } from '@react-three/postprocessing';
 
 export default function Experience() {
   return (
     <>
+      <color args={['#111111']} attach="background" />
+
+      <EffectComposer multisampling={0}>
+        {/*<Vignette blendFunction={BlendFunction.NORMAL} darkness={0.9} offset={0.3} />*/}
+        {/*<Glitch delay={[0.5, 1]} duration={[0.1, 0.3]} mode={GlitchMode.CONSTANT_WILD} strength={[0.2, 0.4]} />*/}
+        {/*<Noise blendFunction={BlendFunction.SOFT_LIGHT} premultiply />*/}
+        <Bloom intensity={1} luminanceThreshold={1} mipmapBlur />
+      </EffectComposer>
+
       <Perf position="top-left" />
 
       <OrbitControls makeDefault />
@@ -16,14 +27,15 @@ export default function Experience() {
         <meshStandardMaterial color="orange" />
       </mesh>
 
+      {/* Bloom */}
       <mesh castShadow position-x={2} scale={1.5}>
         <boxGeometry />
-        <meshStandardMaterial color="mediumpurple" />
+        <meshBasicMaterial color={[1.5, 1, 4]} toneMapped={false} />
       </mesh>
 
       <mesh position-y={-1} receiveShadow rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
-        <meshStandardMaterial color="greenyellow" />
+        <meshStandardMaterial color="#333333" />
       </mesh>
     </>
   );

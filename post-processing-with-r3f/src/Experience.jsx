843e322dd4f0f1d4d161151f96289ad33ff1631b
Evan Lask <elask@logitech.com>
2023-01-30 13:09:35 -0700

    Lesson 50 depth of field

--- post-processing-with-r3f/src/Experience.jsx
+++ post-processing-with-r3f/src/Experience.jsx
@@ -1,18 +1,19 @@
 import { BlendFunction, GlitchMode } from 'postprocessing';
 import { Perf } from 'r3f-perf';
 import { OrbitControls } from '@react-three/drei';
-import { Bloom, EffectComposer, Glitch, Noise, Vignette } from '@react-three/postprocessing';
+import { Bloom, DepthOfField, EffectComposer, Glitch, Noise, Vignette } from '@react-three/postprocessing';
 
 export default function Experience() {
   return (
     <>
-      <color args={['#111111']} attach="background" />
+      <color args={['#ffffff']} attach="background" />
 
       <EffectComposer multisampling={0}>
         {/*<Vignette blendFunction={BlendFunction.NORMAL} darkness={0.9} offset={0.3} />*/}
         {/*<Glitch delay={[0.5, 1]} duration={[0.1, 0.3]} mode={GlitchMode.CONSTANT_WILD} strength={[0.2, 0.4]} />*/}
         {/*<Noise blendFunction={BlendFunction.SOFT_LIGHT} premultiply />*/}
-        <Bloom intensity={1} luminanceThreshold={1} mipmapBlur />
+        {/*<Bloom intensity={1} luminanceThreshold={1} mipmapBlur />*/}
+        <DepthOfField bokehScale={6} focalLength={0.025} focusDistance={0.025} />
       </EffectComposer>
 
       <Perf position="top-left" />
@@ -27,15 +28,14 @@ export default function Experience() {
         <meshStandardMaterial color="orange" />
       </mesh>
 
-      {/* Bloom */}
       <mesh castShadow position-x={2} scale={1.5}>
         <boxGeometry />
-        <meshBasicMaterial color={[1.5, 1, 4]} toneMapped={false} />
+        <meshStandardMaterial color="mediumpurple" />
       </mesh>
 
       <mesh position-y={-1} receiveShadow rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
-        <meshStandardMaterial color="#333333" />
+        <meshStandardMaterial color="greenyellow" />
       </mesh>
     </>
   );

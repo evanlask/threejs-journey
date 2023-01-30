9ae26617731e0814e30029ae52b328a737e571e2
Evan Lask <elask@logitech.com>
2023-01-30 16:27:45 -0700

    Lesson 52 trimesh

--- physics-with-r3f/src/Experience.jsx
+++ physics-with-r3f/src/Experience.jsx
@@ -1,4 +1,5 @@
 import { OrbitControls } from '@react-three/drei';
+import { Debug, Physics, RigidBody } from '@react-three/rapier';
 import { Perf } from 'r3f-perf';
 
 export default function Experience() {
@@ -11,20 +12,30 @@ export default function Experience() {
       <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
 
-      <mesh castShadow position={[-2, 2, 0]}>
-        <sphereGeometry />
-        <meshStandardMaterial color="orange" />
-      </mesh>
+      <Physics>
+        <Debug />
 
-      <mesh castShadow position={[2, 2, 0]}>
-        <boxGeometry />
-        <meshStandardMaterial color="mediumpurple" />
-      </mesh>
+        <RigidBody colliders="ball">
+          <mesh castShadow position={[0, 4, 0]}>
+            <sphereGeometry />
+            <meshStandardMaterial color="orange" />
+          </mesh>
+        </RigidBody>
 
-      <mesh receiveShadow position-y={-1.25}>
-        <boxGeometry args={[10, 0.5, 10]} />
-        <meshStandardMaterial color="greenyellow" />
-      </mesh>
+        <RigidBody colliders="trimesh">
+          <mesh castShadow position={[0, 1, 0]} rotation={[Math.PI * 0.5, 0, 0]}>
+            <torusGeometry args={[1, 0.5, 16, 32]} />
+            <meshStandardMaterial color="mediumpurple" />
+          </mesh>
+        </RigidBody>
+
+        <RigidBody type="fixed">
+          <mesh receiveShadow position-y={-1.25}>
+            <boxGeometry args={[10, 0.5, 10]} />
+            <meshStandardMaterial color="greenyellow" />
+          </mesh>
+        </RigidBody>
+      </Physics>
     </>
   );
 }

3e2a9a076a85c1a12e1ed8d51d7001fbbd99b867
Evan Lask <elask@logitech.com>
2023-01-31 12:02:29 -0700

    Lesson 42 sound

--- physics-with-r3f/src/Experience.jsx
+++ physics-with-r3f/src/Experience.jsx
@@ -1,17 +1,30 @@
 import { OrbitControls } from '@react-three/drei';
+import { useFrame, useThree } from '@react-three/fiber';
 import { BallCollider, CuboidCollider, Debug, Physics, RigidBody } from '@react-three/rapier';
 import { Perf } from 'r3f-perf';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import * as THREE from 'three';
 
 export default function Experience() {
   const cube = useRef();
+  const twister = useRef();
+
+  const hitSound = useRef(new Audio('./hit.mp3'));
+
+  const collisionEnter = (e) => {
+    hitSound.current.currentTime = 0;
+    hitSound.current.volume = Math.random();
+    hitSound.current.play();
+  };
 
   const cubeJump = (e) => {
     console.log(e, cube.current);
 
+    const mass = cube.current.mass();
+
     cube.current.applyImpulse({
-      x: -2,
-      y: 5,
+      x: 0,
+      y: 5 * mass,
       z: 0,
     });
 
@@ -22,6 +35,25 @@ export default function Experience() {
     });
   };
 
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime();
+
+    const eulerRotation = new THREE.Euler(0, time * 3, 0);
+    const quaternionRotation = new THREE.Quaternion();
+    quaternionRotation.setFromEuler(eulerRotation);
+
+    const angle = time * 0.5;
+    const x = Math.cos(angle) * 2;
+    const y = Math.sin(angle) * 2;
+
+    twister.current.setNextKinematicRotation(quaternionRotation);
+    twister.current.setNextKinematicTranslation({
+      x: x,
+      y: -0.8,
+      z: y,
+    });
+  });
+
   return (
     <>
       <Perf position="top-left" />
@@ -31,7 +63,7 @@ export default function Experience() {
       <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
 
-      <Physics>
+      <Physics gravity={[0, -9.81, 0]}>
         <Debug />
 
         <RigidBody colliders="ball">
@@ -41,10 +73,26 @@ export default function Experience() {
           </mesh>
         </RigidBody>
 
-        <RigidBody position={[1.5, 2, 0]} ref={cube}>
+        <RigidBody
+          colliders={false}
+          friction={0.7}
+          gravityScale={1}
+          onCollisionEnter={collisionEnter}
+          position={[1.5, 2, 0]}
+          ref={cube}
+          restitution={0}
+        >
           <mesh castShadow onClick={cubeJump}>
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
+          </mesh>
+          <CuboidCollider args={[0.5, 0.5, 0.5]} mass={5} />
+        </RigidBody>
+
+        <RigidBody friction={0} position={[0, -0.8, 0]} ref={twister} type="kinematicPosition">
+          <mesh cashShadow scale={[0.4, 0.4, 3]}>
+            <boxGeometry />
+            <meshStandardMaterial color="red" />
           </mesh>
         </RigidBody>
 
@@ -58,7 +106,7 @@ export default function Experience() {
         </RigidBody>
         */}
 
-        <RigidBody type="fixed">
+        <RigidBody friction={0.7} type="fixed">
           <mesh receiveShadow position-y={-1.25}>
             <boxGeometry args={[10, 0.5, 10]} />
             <meshStandardMaterial color="greenyellow" />

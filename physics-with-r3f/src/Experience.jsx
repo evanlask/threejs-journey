de3df623e8bd19f239d82df629eee8fb0fb01925
Evan Lask <elask@logitech.com>
2023-01-31 14:10:20 -0700

    Lesson 52 complete

--- physics-with-r3f/src/Experience.jsx
+++ physics-with-r3f/src/Experience.jsx
@@ -1,9 +1,18 @@
-import { OrbitControls } from '@react-three/drei';
+import { useGLTF, OrbitControls } from '@react-three/drei';
 import { useFrame, useThree } from '@react-three/fiber';
-import { BallCollider, CuboidCollider, Debug, Physics, RigidBody } from '@react-three/rapier';
+import {
+  BallCollider,
+  CuboidCollider,
+  CylinderCollider,
+  Debug,
+  InstancedRigidBodies,
+  Physics,
+  RigidBody,
+} from '@react-three/rapier';
 import { Perf } from 'r3f-perf';
-import { useRef, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import * as THREE from 'three';
+import { MeshStandardMaterial } from 'three';
 
 export default function Experience() {
   const cube = useRef();
@@ -11,15 +20,15 @@ export default function Experience() {
 
   const hitSound = useRef(new Audio('./hit.mp3'));
 
+  const hamburger = useGLTF('./hamburger.glb');
+
   const collisionEnter = (e) => {
-    hitSound.current.currentTime = 0;
-    hitSound.current.volume = Math.random();
-    hitSound.current.play();
+    // hitSound.current.currentTime = 0;
+    // hitSound.current.volume = Math.random();
+    // hitSound.current.play();
   };
 
   const cubeJump = (e) => {
-    console.log(e, cube.current);
-
     const mass = cube.current.mass();
 
     cube.current.applyImpulse({
@@ -54,6 +63,46 @@ export default function Experience() {
     });
   });
 
+  const cubes = useRef();
+  const cubeCount = 600;
+
+  // useEffect(() => {
+  //   for (let i = 0; i < cubeCount; i++) {
+  //     const matrix = new THREE.Matrix4();
+  //     matrix.compose(
+  //       new THREE.Vector3(i * 2, 0, 0), //
+  //       new THREE.Quaternion(),
+  //       new THREE.Vector3(1, 1, 1)
+  //     );
+  //     cubes.current.setMatrixAt(i, matrix);
+  //   }
+  // }, []);
+
+  const cubeTransforms = useMemo(() => {
+    const positions = [];
+    const rotations = [];
+    const scales = [];
+
+    for (let i = 0; i < cubeCount; i++) {
+      positions.push([
+        (Math.random() - 0.5) * 6, //
+        6 + i * 0.5,
+        (Math.random() - 0.5) * 6,
+      ]);
+
+      rotations.push([
+        Math.random(), //
+        Math.random(),
+        Math.random(),
+      ]);
+
+      const scale = 0.2 + Math.random() * 0.8;
+      scales.push([scale, scale, scale]);
+    }
+
+    return { positions, rotations, scales };
+  }, []);
+
   return (
     <>
       <Perf position="top-left" />
@@ -64,7 +113,7 @@ export default function Experience() {
       <ambientLight intensity={0.5} />
 
       <Physics gravity={[0, -9.81, 0]}>
-        <Debug />
+        {/*<Debug />*/}
 
         <RigidBody colliders="ball">
           <mesh castShadow position={[-1.5, 2, 0]}>
@@ -78,6 +127,15 @@ export default function Experience() {
           friction={0.7}
           gravityScale={1}
           onCollisionEnter={collisionEnter}
+          onCollisionExit={() => {
+            /*console.log('exit');*/
+          }}
+          onSleep={() => {
+            console.log('sleep');
+          }}
+          onWake={() => {
+            console.log('wake');
+          }}
           position={[1.5, 2, 0]}
           ref={cube}
           restitution={0}
@@ -90,7 +148,7 @@ export default function Experience() {
         </RigidBody>
 
         <RigidBody friction={0} position={[0, -0.8, 0]} ref={twister} type="kinematicPosition">
-          <mesh cashShadow scale={[0.4, 0.4, 3]}>
+          <mesh cashShadow scale={[0.4, 0.4, 4]}>
             <boxGeometry />
             <meshStandardMaterial color="red" />
           </mesh>
@@ -112,6 +170,29 @@ export default function Experience() {
             <meshStandardMaterial color="greenyellow" />
           </mesh>
         </RigidBody>
+
+        <RigidBody type="fixed">
+          <CuboidCollider args={[5, 2, 0.5]} position={[0, 1, 5.5]} />
+          <CuboidCollider args={[5, 2, 0.5]} position={[0, 1, -5.5]} />
+          <CuboidCollider args={[0.5, 2, 5]} position={[5.5, 1, 0]} />
+          <CuboidCollider args={[0.5, 2, 5]} position={[-5.5, 1, 0]} />
+        </RigidBody>
+
+        <RigidBody colliders={false} position={[0, 5, 0]}>
+          <primitive object={hamburger.scene} scale={0.25} />
+          <CylinderCollider args={[0.5, 1.25]} />
+        </RigidBody>
+
+        <InstancedRigidBodies
+          positions={cubeTransforms.positions}
+          rotations={cubeTransforms.rotations}
+          scales={cubeTransforms.scales}
+        >
+          <instancedMesh args={[null, null, cubeCount]} castShadow ref={cubes}>
+            <boxGeometry />
+            <meshStandardMaterial color="tomato" />
+          </instancedMesh>
+        </InstancedRigidBodies>
       </Physics>
     </>
   );
